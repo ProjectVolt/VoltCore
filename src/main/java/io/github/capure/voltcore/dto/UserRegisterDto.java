2c214f8efbe7a1b3cc8d6b4a86d790cad232211b
Kacper Stolarek <stolarek83@gmail.com>
2024-03-13 14:52:56 +0100

    Added username pattern validation

--- src/main/java/io/github/capure/voltcore/dto/UserRegisterDto.java
+++ src/main/java/io/github/capure/voltcore/dto/UserRegisterDto.java
@@ -2,6 +2,7 @@ package io.github.capure.voltcore.dto;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -9,14 +10,18 @@ import lombok.Data;
 @Data
 @AllArgsConstructor
 public class UserRegisterDto {
-    @NotNull @Size(min=3, max=50)
+    @NotNull
+    @Size(min = 3, max = 50)
+    @Pattern(regexp = "^[a-zA-Z0-9]+$")
     private String username;
-    @NotNull @Size(min=8, max=250)
+    @NotNull
+    @Size(min = 8, max = 250)
     private String password;
-    @NotNull @Email(regexp = "^[a-zA-Z0-9_!#$%&'*+/=?``{|}~^.-]+@[a-zA-Z0-9.-]+$", message = "Please provide a valid email address")
+    @NotNull
+    @Email(regexp = "^[a-zA-Z0-9_!#$%&'*+/=?``{|}~^.-]+@[a-zA-Z0-9.-]+$", message = "Please provide a valid email address")
     private String email;
-    @Size(max=50)
+    @Size(max = 50)
     private String github;
-    @Size(max=100)
+    @Size(max = 100)
     private String school;
 }
